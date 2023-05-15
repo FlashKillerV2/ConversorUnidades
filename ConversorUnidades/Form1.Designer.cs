fda69ecc833528ca9c3a046040ab9e2f4590a902
FlashKillerV <sebasromtz123@gmail.com>
2023-05-14 21:47:16 -0600

    rediseñado

--- ConversorUnidades/Form1.Designer.cs
+++ ConversorUnidades/Form1.Designer.cs
@@ -31,8 +31,8 @@ namespace ConversorUnidades
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.label1 = new System.Windows.Forms.Label();
             this.txtcant = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -44,22 +44,31 @@ namespace ConversorUnidades
             this.btnCalcular = new System.Windows.Forms.Button();
             this.txtResultado = new System.Windows.Forms.TextBox();
             this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
-            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(102)))));
+            this.toolStrip1.BackColor = System.Drawing.Color.LightSlateGray;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripLabel1,
             this.toolStripButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(332, 27);
+            this.toolStrip1.Size = new System.Drawing.Size(925, 27);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "Conversor de unidades";
             this.toolStrip1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.toolStrip1_MouseDown);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.ForeColor = System.Drawing.Color.White;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(128, 24);
+            this.toolStripLabel1.Text = "Conversor de unidades";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
             // 
             // toolStripButton1
             // 
@@ -71,14 +80,6 @@ namespace ConversorUnidades
             this.toolStripButton1.Size = new System.Drawing.Size(24, 24);
             this.toolStripButton1.Text = "Cerrar";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
-            // 
-            // toolStripLabel1
-            // 
-            this.toolStripLabel1.ForeColor = System.Drawing.Color.White;
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(128, 24);
-            this.toolStripLabel1.Text = "Conversor de unidades";
-            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
             // 
             // label1
             // 
@@ -94,8 +95,8 @@ namespace ConversorUnidades
             // txtcant
             // 
             this.txtcant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtcant.Location = new System.Drawing.Point(67, 84);
-            this.txtcant.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtcant.Location = new System.Drawing.Point(285, 44);
+            this.txtcant.Margin = new System.Windows.Forms.Padding(2);
             this.txtcant.Name = "txtcant";
             this.txtcant.Size = new System.Drawing.Size(203, 24);
             this.txtcant.TabIndex = 2;
@@ -105,7 +106,7 @@ namespace ConversorUnidades
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(125, 153);
+            this.label2.Location = new System.Drawing.Point(11, 117);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 21);
@@ -116,7 +117,7 @@ namespace ConversorUnidades
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(152, 216);
+            this.label3.Location = new System.Drawing.Point(258, 118);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 21);
@@ -127,7 +128,7 @@ namespace ConversorUnidades
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(156, 288);
+            this.label4.Location = new System.Drawing.Point(436, 119);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(20, 21);
@@ -138,10 +139,10 @@ namespace ConversorUnidades
             // 
             this.cbMagnitudes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbMagnitudes.FormattingEnabled = true;
-            this.cbMagnitudes.Location = new System.Drawing.Point(84, 175);
-            this.cbMagnitudes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbMagnitudes.Location = new System.Drawing.Point(107, 118);
+            this.cbMagnitudes.Margin = new System.Windows.Forms.Padding(2);
             this.cbMagnitudes.Name = "cbMagnitudes";
-            this.cbMagnitudes.Size = new System.Drawing.Size(169, 21);
+            this.cbMagnitudes.Size = new System.Drawing.Size(147, 21);
             this.cbMagnitudes.TabIndex = 6;
             this.cbMagnitudes.SelectedIndexChanged += new System.EventHandler(this.cbMagnitudes_SelectedIndexChanged);
             // 
@@ -149,20 +150,20 @@ namespace ConversorUnidades
             // 
             this.cbDe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbDe.FormattingEnabled = true;
-            this.cbDe.Location = new System.Drawing.Point(84, 238);
-            this.cbDe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbDe.Location = new System.Drawing.Point(285, 119);
+            this.cbDe.Margin = new System.Windows.Forms.Padding(2);
             this.cbDe.Name = "cbDe";
-            this.cbDe.Size = new System.Drawing.Size(169, 21);
+            this.cbDe.Size = new System.Drawing.Size(147, 21);
             this.cbDe.TabIndex = 7;
             // 
             // cbA
             // 
             this.cbA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbA.FormattingEnabled = true;
-            this.cbA.Location = new System.Drawing.Point(84, 309);
-            this.cbA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbA.Location = new System.Drawing.Point(460, 120);
+            this.cbA.Margin = new System.Windows.Forms.Padding(2);
             this.cbA.Name = "cbA";
-            this.cbA.Size = new System.Drawing.Size(169, 21);
+            this.cbA.Size = new System.Drawing.Size(147, 21);
             this.cbA.TabIndex = 8;
             // 
             // btnCalcular
@@ -171,8 +172,8 @@ namespace ConversorUnidades
             this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalcular.ForeColor = System.Drawing.Color.White;
-            this.btnCalcular.Location = new System.Drawing.Point(116, 354);
-            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalcular.Location = new System.Drawing.Point(262, 195);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(106, 31);
             this.btnCalcular.TabIndex = 9;
@@ -184,8 +185,8 @@ namespace ConversorUnidades
             // 
             this.txtResultado.Enabled = false;
             this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResultado.Location = new System.Drawing.Point(67, 392);
-            this.txtResultado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtResultado.Location = new System.Drawing.Point(694, 117);
+            this.txtResultado.Margin = new System.Windows.Forms.Padding(2);
             this.txtResultado.Name = "txtResultado";
             this.txtResultado.Size = new System.Drawing.Size(203, 24);
             this.txtResultado.TabIndex = 10;
@@ -196,8 +197,8 @@ namespace ConversorUnidades
             this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLimpiar.ForeColor = System.Drawing.Color.White;
-            this.btnLimpiar.Location = new System.Drawing.Point(116, 444);
-            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLimpiar.Location = new System.Drawing.Point(440, 195);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(106, 31);
             this.btnLimpiar.TabIndex = 11;
@@ -205,12 +206,24 @@ namespace ConversorUnidades
             this.btnLimpiar.UseVisualStyleBackColor = false;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(611, 120);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Resultado";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(332, 494);
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(925, 310);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.btnCalcular);
@@ -225,7 +238,7 @@ namespace ConversorUnidades
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conversor de unidades";
@@ -253,6 +266,7 @@ namespace ConversorUnidades
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.TextBox txtResultado;
         private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label5;
     }
 }
 
